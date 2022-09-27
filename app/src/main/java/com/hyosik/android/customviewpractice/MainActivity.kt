7dd6bd0e88bad4795437182e152c84b54e23422c
hyosikchoi <huchoi24@naver.com>
2022-09-28 01:34:06 +0900

    Practice BottomSheetDialog

--- app/src/main/java/com/hyosik/android/customviewpractice/MainActivity.kt
+++ app/src/main/java/com/hyosik/android/customviewpractice/MainActivity.kt
@@ -2,10 +2,26 @@ package com.hyosik.android.customviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.hyosik.android.customviewpractice.bottomsheet.CustomBottomSheet
+import com.hyosik.android.customviewpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    private val customBottomSheet = CustomBottomSheet()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        initView()
     }
+
+    private fun initView() = with(binding) {
+        btnShowBottomSheet.setOnClickListener {
+            customBottomSheet.show(supportFragmentManager, customBottomSheet.tag)
+        }
+    }
+
 }
---
+++ app/src/main/java/com/hyosik/android/customviewpractice/bottomsheet/CustomBottomSheet.kt
@@ -0,0 +1,25 @@
+package com.hyosik.android.customviewpractice.bottomsheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.hyosik.android.customviewpractice.R
+import com.hyosik.android.customviewpractice.databinding.DialogBottomSheetBinding
+
+class CustomBottomSheet() : BottomSheetDialogFragment() {
+
+    private lateinit var binding : DialogBottomSheetBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,R.layout.dialog_bottom_sheet,container,false)
+        return binding.root
+    }
+
+}
